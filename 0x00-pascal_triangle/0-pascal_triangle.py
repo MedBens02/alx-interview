93545baa7476e47bafae085e29c0d39076462edc
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-07-26 00:47:05 +0000

    pascal

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+def pascal_triangle(n):
+    """
+        returns a list of lists of integers representing
+        the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        line = [1]
+        for j in range(1, i):
+            line.append(triangle[i-1][j-1] + triangle[i-1][j])
+        line.append(1)
+        triangle.append(line)
+
+    return triangle
