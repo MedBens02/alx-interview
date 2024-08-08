5bd0f03190d33faf61568ed3880f552302e92280
Mohamed Bensaddik <mohamedbensaddik1234@gmail.com>
2024-08-08 22:04:20 +0000

    0

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Minimum operations challenge.
+'''
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    oper = 0
+    fact = 2
+
+    while n > 1:
+        while n % fact == 0:
+            oper += fact
+            n //= fact
+        fact += 1
+
+    return oper
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,1 @@
+Interview proj 2
